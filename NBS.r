0ff103436a80a4b54a377da539a1babae4bd9013
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-01-02 16:21:02 +0800

    Update NBS.r

--- NBS.r
+++ NBS.r
@@ -83,8 +83,8 @@ NBS=function(all_predictors,IV_of_interest, FC_data, nperm=50, nthread=1, p=0.00
       IV_of_interest=IV_of_interest[-idxF]
       FC_data=FC_data[-idxF,]
     }
-  
-    #identify contrast
+
+    #check IV_of_interest
     for(colno in 1:(NCOL(all_predictors)+1))
     {
       if(colno==(NCOL(all_predictors)+1))  {stop("IV_of_interest is not contained within all_predictors")}
