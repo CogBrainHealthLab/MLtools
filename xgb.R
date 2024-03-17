b12561f904c8344e185b443f5d950a2a633944b2
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-03-18 02:14:08 +0800

    Update xgb.R

--- xgb.R
+++ xgb.R
@@ -20,6 +20,7 @@ XGBlinear=function(train_dat,outcome, alpharange=c(0,1), lambdarange=c(1,5),etar
   bounds = list(lambda = lambdarange,alpha = alpharange,eta = etarange)
   dtrain = xgboost::xgb.DMatrix(data = train_dat, label = outcome)
   doParallel::registerDoParallel(nthread)
+  cl=parallel::makeCluster(nthread)
   parallel::clusterExport(cl,c('train_dat','outcome','folds'),envir = environment())
 
   obj_func = function(eta,lambda,alpha,nround) 
